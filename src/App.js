23d181ed48249def8a77c3af5011918d955408b7
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-11-30 20:57:37 +0530

    const ACTIONS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { useReducer } from "react";
 import "./styles.css";
 
-function reducer(state, action) {}
+const ACTIONS = {
+  ADD_DIGIT: "add-digit",
+  CHOOSE_OPERATION: "choose-operation",
+  CLEAR: "clear",
+  DELETE_DIGIT: "delete-digit",
+  EVALUATE: "evaluate",
+};
+
+function reducer(state, { type, payload }) {}
 
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
