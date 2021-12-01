28b00b70713ad0510c88263fc6f8b46a89e8a0eb
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-01 18:58:47 +0530

    case ACTIONS.DELETE_DIGIT

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, { type, payload }) {
   switch (type) {
     case ACTIONS.ADD_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        };
+      }
       if (payload.digit === "0" && state.currentOperand === "0") {
         return state;
       }
@@ -53,6 +60,14 @@ function reducer(state, { type, payload }) {
       };
     case ACTIONS.CLEAR:
       return {};
+    case ACTIONS.DELETE_DIGIT:
+      if (state.overwrite) {
+        return {
+          ...state,
+          overwrite: false,
+          currentOperand: null,
+        };
+      }
     case ACTIONS.EVALUATE:
       if (
         state.operation == null ||
