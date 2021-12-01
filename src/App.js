d5486aa5a16f3a4f88f93187974b0a38ad7e4a95
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-01 19:07:29 +0530

    INTEGER_FORMATTER

--- src/App.js
+++ src/App.js
@@ -118,6 +118,10 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   return computation.toString();
 }
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+});
+
 function App() {
   const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(
     reducer,
