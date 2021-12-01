d59031add9f0aa4404c38700e085ac22e9db3c46
Deshan Vimukthi <deshanvimukthi59@gmail.com>
2021-12-01 18:35:59 +0530

    evaluate switch

--- src/App.js
+++ src/App.js
@@ -53,6 +53,23 @@ function evaluate({ currentOperand, previousOperand, operation }) {
   const prev = parseFloat(previousOperand);
   const current = parseFloat(currentOperand);
   if (isNaN(prev) || isNaN(current)) return "";
+  let computation = "";
+  switch (operation) {
+    case "+":
+      computation = prev + current;
+      break;
+    case "-":
+      computation = prev - current;
+      break;
+    case "*":
+      computation = prev * current;
+      break;
+    case "/":
+      computation = prev / current;
+      break;
+  }
+
+  return computation.toString();
 }
 
 function App() {
